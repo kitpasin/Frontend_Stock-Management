bff0e59ca2dee27074baa2b8d87e8b88e59d014a
kitpasin <kitpasin.ice@gmail.com>
2023-06-16 14:07:50 +0700

    dashboard

---
+++ src/pages/dashboard/components/Summaries.jsx
@@ -0,0 +1,258 @@
+import React from 'react'
+import { Card } from '@mui/material';
+
+function Summaries() {
+  return (
+    <>
+      <div className="grid-container-1fr-1fr-white">
+        {/* summaries 1 */}
+        <Card className="item">
+          <div className="content">
+            <figure className='image'>
+              <img src="/images/icons/product-icon.png" alt="Product-icon" />
+            </figure>
+            <div className="text">
+              <div className="text-title">
+                <p>รวมรายการ สินค้าไกล้หมด</p>
+              </div>
+              <div className="text-description">
+                <p>90,000 รายการ / 85,324 หน่วย</p>
+              </div>
+            </div>
+          </div>
+          <p>|</p>
+          <div className="content">
+            <div className="text">
+              <div className="text-title">
+                <p>น้อยสุดคือ : อาหาร</p>
+              </div>
+              <div className="text-description">
+                <p>15 หน่วย</p>
+              </div>
+            </div>
+            <button className="graph">แสดงกราฟ</button>
+          </div>
+        </Card>
+
+        {/* summaries 2 */}
+        <Card className="item">
+          <div className="content">
+            <figure className="image">
+              <img src="/images/icons/expire-icon.png" alt="Expire-icon" />
+            </figure>
+            <div className="text">
+              <div className="text-title">
+                <p>รวมรายการ สินค้าไกล้หมด</p>
+              </div>
+              <div className="text-description">
+                <p>90,000 รายการ / 85,324 หน่วย</p>
+              </div>
+            </div>
+          </div>
+          <p>|</p>
+          <div className="content">
+            <div className="text">
+              <div className="text-title">
+                <p>มากสุดคือ : เครื่องดื่ม</p>
+              </div>
+              <div className="text-description">
+                <p>240 หน่วย</p>
+              </div>
+            </div>
+            <button className="graph">แสดงกราฟ</button>
+          </div>
+        </Card>
+
+        {/* summaries 3 */}
+        <Card className="item">
+          <div className="content">
+            <figure className="image">
+              <img src="/images/icons/export-icon.png" alt="Product-icon" />
+            </figure>
+            <div className="text">
+              <div className="text-title">
+                <p>รวมรายการ เบิกออกล่าสุด</p>
+              </div>
+              <div className="text-description">
+                <p>25,000 รายการ / 10,542 หน่วย</p>
+              </div>
+            </div>
+          </div>
+          <p>|</p>
+          <div className="content">
+            <div className="text">
+              <div className="text-title">
+                <p>มากสุดคือ : มาม่า</p>
+              </div>
+              <div className="text-description">
+                <p>550 หน่วย</p>
+              </div>
+            </div>
+            <button className="graph">แสดงกราฟ</button>
+          </div>
+        </Card>
+
+        {/* summaries 4 */}
+        <Card className="item">
+          <div className="content">
+            <figure className="image">
+              <img src="/images/icons/import-icon.png" alt="Expire-icon" />
+            </figure>
+            <div className="text">
+              <div className="text-title">
+                <p>รวมรายการ นำเข้าล่าสุด</p>
+              </div>
+              <div className="text-description">
+                <p>25,000 รายการ / 10,542 หน่วย</p>
+              </div>
+            </div>
+          </div>
+          <p>|</p>
+          <div className="content">
+            <div className="text">
+              <div className="text-title">
+                <p>มากสุดคือ : ขนม</p>
+              </div>
+              <div className="text-description">
+                <p>550 หน่วย</p>
+              </div>
+            </div>
+            <button className="graph">แสดงกราฟ</button>
+          </div>
+        </Card>
+      </div>
+
+      <div className="flex-container-between">
+        <div className="grid-container-1fr-1fr-purple">
+          {/* summaries 5 */}
+          <Card className="item">
+            <p className="header">สินค้า เบิกออกมากที่สุด/เดือน</p>
+            <div className="content">
+              <figure className="image">
+                <img src="/images/mock/product1.png" alt="" />
+              </figure>
+              <div className="wrapper">
+                <p className="title">เลย์ล็อคแผ่นหยักรส ออริจิ นอล บิก แพ็ค...</p>
+                <div>
+                  <div className="description">
+                    <p>ปริมาณสุทธิ</p>
+                    <p>150 gm</p>
+                  </div>
+                  <div className="description">
+                    <p>ราคาขาย</p>
+                    <p>100 THB</p>
+                  </div>
+                  <div className="description">
+                    <p>วันเบิก</p>
+                    <p>28 sep 2023</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="summary">
+              <p>จำนวนเบิกออก</p>
+              <p>150 หน่วย</p>
+            </div>
+          </Card>
+
+          {/* summaries 6 */}
+          <Card className="item">
+            <p className="header">สินค้า คงเหลือในสต็อกมากสุด</p>
+            <div className="content">
+              <figure className="image">
+                <img src="/images/mock/product1.png" alt="" />
+              </figure>
+              <div className="wrapper">
+                <p className="title">มาม่าแกงเขียวหวานไก่</p>
+                <div>
+                  <div className="description">
+                    <p>ปริมาณสุทธิ</p>
+                    <p>150 gm</p>
+                  </div>
+                  <div className="description">
+                    <p>ราคาขาย</p>
+                    <p>100 THB</p>
+                  </div>
+                  <div className="description">
+                    <p>วันเบิก</p>
+                    <p>28 sep 2023</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="summary">
+              <p>คงเหลือ</p>
+              <p>4500 หน่วย</p>
+            </div>
+          </Card>
+        </div>
+
+        <div className="grid-container-1fr-1fr-orange">
+          {/* summaries 7 */}
+          <Card className="item">
+            <p className="header">สินค้าไกลหมดอายุ</p>
+            <div className="content">
+              <figure className="image">
+                <img src="/images/mock/product1.png" alt="" />
+              </figure>
+              <div className="wrapper">
+                <p className="title">กูลิโกะ ป๊อกกี้ บิสกิตแท่ง เคลือบช็อกโกแ...</p>
+                <div>
+                  <div className="description">
+                    <p>ปริมาณสุทธิ</p>
+                    <p>150 gm</p>
+                  </div>
+                  <div className="description">
+                    <p>ราคาขาย</p>
+                    <p>100 THB</p>
+                  </div>
+                  <div className="description">
+                    <p>วันเบิก</p>
+                    <p>28 sep 2023</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="summary">
+              <p>จำนวน</p>
+              <p>150 หน่วย</p>
+            </div>
+          </Card>
+
+          {/* Summaries 8 */}
+          <Card className="item">
+            <p className="header">สินค้าไกล้หมด สต็อก</p>
+            <div className="content">
+              <figure className="image">
+                <img src="/images/mock/product1.png" alt="" />
+              </figure>
+              <div className="wrapper">
+                <p className="title">มาม่าคัฟรสต้มยำกุ้ง</p>
+                <div>
+                  <div className="description">
+                    <p>ปริมาณสุทธิ</p>
+                    <p>150 gm</p>
+                  </div>
+                  <div className="description">
+                    <p>ราคาขาย</p>
+                    <p>100 THB</p>
+                  </div>
+                  <div className="description">
+                    <p>วันเบิก</p>
+                    <p>28 sep 2023</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="summary">
+              <p>จำนวน</p>
+              <p>10 หน่วย</p>
+            </div>
+          </Card>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Summaries
--- src/pages/dashboard/dashboard.js
+++ src/pages/dashboard/dashboard.js
@@ -1,102 +1,24 @@
+/* eslint-disable */
 import React, { useEffect, useRef, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useDispatch } from "react-redux";
 
 import "./dashboard.scss";
-import {  faGamepad,faRedo } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import ButtonUI from "../../components/ui/button/button";
-import HeadPageComponent from "../../components/layout/headpage/headpage";
-import OrderTable from "./order-tab";
-import { getBookingList } from "../../services/booking.service";
-import { appActions } from "../../store/app-slice";
-import BookingSettings from "./settings";
+import Summaries from "./components/Summaries";
+import Tables from "./components/Tables";
 
 const DashboardPage = () => {
   const { t } = useTranslation(["dashboard-page"]);
   const dispatch = useDispatch();
-  const [refreshData, setRefreshData] = useState(0);
-  const [orderData, setOrderData] = useState([]);
-  const [filteredData, setFIlteredData] = useState([]);
-  const [settings, setSettings] = useState({
-    numberPeople: 1,
-    timesAvailable: "",
-    disabledDay: "",
-    disabledDate: "",
-    disabledHoliday: ""
-  });
 
   useEffect(() => {
-    // dispatch(appActions.isSpawnActive(true));
-    // getBookingList().then((res) => {
-    //   setSettings({
-    //     numberPeople: res.setting.people_number,
-    //     timesAvailable: res.setting.available_time ,
-    //     disabledDay:  res.setting.disable_by_day ,
-    //     disabledDate:  res.setting.disable_by_date ,
-    //     disabledHoliday: res.setting.special_holiday ,
-    //   })
-    //   const result = res.data.map((d) => {
-    //     return {
-    //       id: d.id,
-    //       name: d.firstname + " " + d.surname,
-    //       request: d.specific_request,
-    //       bookingDate: d.time_booking,
-    //       amount: d.people_number,
-    //       forgroup: d.forgroup,
-    //       email: d.email,
-    //       phone: d.phone,
-    //       status: d.status,
-    //       created_at: d.created_at,
-    //       updated_at: d.updated_at 
-    //     }
-    //   })
-
-    //   dispatch(appActions.isSpawnActive(false));
-    //   setOrderData(result);
-    //   setFIlteredData(result);
-    // });
-  }, [refreshData]);
-
-    const filterData = () => {
-        const filted = orderData.filter((f) => {
-            return f
-        });
-        setFIlteredData(filted);
-    }
-
+    
+  }, []);
 
   return (
     <section id="dashboard-page">
-      <HeadPageComponent
-        h1={"dashboardPage"}
-        icon={<FontAwesomeIcon icon={faGamepad} />}
-        breadcrums={[{ title: "dashboardPage", link: false }]}
-      />
-
-      <div className="card-control fixed-width">
-        <div className="card-head">
-          <div className="head-action">
-            <h2 className="head-title">
-              <ButtonUI
-                onClick={() => setRefreshData(refreshData + 1)}
-                on="create"
-                isLoading={false}
-                icon={<FontAwesomeIcon icon={faRedo} />} >
-                {t("Fetch")}
-              </ButtonUI>
-            </h2>
-          </div>
-        </div>
-     
-        <div className="card-body">
-          {/* <OrderTable
-            filteredData={filteredData}
-            setRefreshData={() => setRefreshData(refreshData + 1)}
-          /> */}
-          {/* <BookingSettings setRefreshData={setRefreshData} settings={settings} /> */}
-        </div>
-      </div>
+      <Summaries />
+      <Tables />
     </section>
   );
 };
