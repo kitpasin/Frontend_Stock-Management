ec692b489e7a4fc7d652b891e8071058730e5cad
bnrboyy <nantachai.ru@gmail.com>
2023-07-11 17:12:17 +0700

    update

--- src/pages/products/components/Table.jsx
+++ src/pages/products/components/Table.jsx
@@ -5,18 +5,31 @@ import { Button } from "@mui/material";
 import { Menu } from "@mui/material";
 import { MenuItem } from "@mui/material";
 import { useSelector } from "react-redux";
+
+import MenuItemList from "./MenuItemList";
 import dayjs from "dayjs";
+
+
 
 function Table({ rows, productsAll }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [openMenu, setOpenMenu] = React.useState(false);
+  const [btnId, setbtnId] = React.useState(false);
   const open = Boolean(anchorEl);
   const webPath = useSelector((state) => state.app.webPath);
 
   function handleClick(event) {
+    setbtnId(event.currentTarget.getAttribute("id"))
     setAnchorEl(event.currentTarget);
+    setOpenMenu(true)
   }
   function handleClose() {
     setAnchorEl(null);
+    setOpenMenu(false)
+  }
+
+  const editHandle = (product_id) => {
+    console.log(product_id)
   }
 
   const columns = [
@@ -402,10 +415,10 @@ function Table({ rows, productsAll }) {
       renderCell: (params) => (
         <div>
           <Button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
+            id={`basic-button${params.row.id}`}
+            aria-controls={openMenu ? `basic-menu${params.row.id}` : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
+            aria-expanded={openMenu ? "true" : undefined}
             onClick={handleClick}
           >
             <img
@@ -420,69 +433,69 @@ function Table({ rows, productsAll }) {
               alt=""
             />
           </Button>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem
-              sx={{
-                display: "flex",
-                gap: "1rem",
+          <MenuItemList params={params} anchorEl={anchorEl} open={openMenu} handleClose={handleClose} buttonId={btnId} />
+          {/* <Menu
+              id="basic-menu"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-button",
               }}
-              onClick={handleClose}
             >
-              <img
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  filter:
-                    "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
+              <MenuItem
+                sx={{
+                  display: "flex",
+                  gap: "1rem",
                 }}
-                src="/images/icons/supplier-icon.png"
-                alt=""
-              />
-              <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>ซัพพลาย</p>
-            </MenuItem>
-            <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={handleClose}>
-              <img
-                style={{ width: "18px", height: "18px" }}
-                src="/images/icons/export-icon.png"
-                alt=""
-              />
-              <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>เบิกสินค้า</p>
-            </MenuItem>
-            <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={handleClose}>
-              <img
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  filter:
-                    "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
-                }}
-                src="/images/icons/edit-icon.png"
-                alt=""
-              />
-              <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>แก้ไขสินค้า</p>
-            </MenuItem>
-            <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={handleClose}>
-              <img
-                style={{
-                  width: "18px",
-                  height: "18px",
-                  filter:
-                    "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
-                }}
-                src="/images/icons/trash-icon.png"
-                alt=""
-              />
-              <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>ลบสินค้า</p>
-            </MenuItem>
-          </Menu>
+                onClick={handleClose}
+              >
+                <img
+                  style={{
+                    width: "18px",
+                    height: "18px",
+                    filter:
+                      "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
+                  }}
+                  src="/images/icons/supplier-icon.png"
+                  alt=""
+                />
+                <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>ซัพพลาย</p>
+              </MenuItem>
+              <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={handleClose}>
+                <img
+                  style={{ width: "18px", height: "18px" }}
+                  src="/images/icons/export-icon.png"
+                  alt=""
+                />
+                <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>เบิกสินค้า</p>
+              </MenuItem>
+              <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={() => editHandle(params.row)}>
+                <img
+                  style={{
+                    width: "18px",
+                    height: "18px",
+                    filter: "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
+                  }}
+                  src="/images/icons/edit-icon.png"
+                  alt=""
+                />
+                <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>แก้ไขสินค้า</p>
+              </MenuItem>
+              <MenuItem sx={{ display: "flex", gap: "1rem" }} onClick={handleClose}>
+                <img
+                  style={{
+                    width: "18px",
+                    height: "18px",
+                    filter:
+                      "invert(85%) sepia(25%) saturate(2350%) hue-rotate(217deg) brightness(95%) contrast(88%)",
+                  }}
+                  src="/images/icons/trash-icon.png"
+                  alt=""
+                />
+                <p style={{ fontSize: "18px", fontWeight: 400, color: "#3B336B" }}>ลบสินค้า</p>
+              </MenuItem>
+          </Menu> */}
         </div>
       ),
     },
